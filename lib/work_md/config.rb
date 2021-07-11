7d7af11e805d37d546752e4f02080710cc0e719b
henriquefernandez <hriqueft@gmail.com>
2021-07-10 21:38:24 -0300

    feature(not-require-config): Remove config.yml requirement

--- lib/work_md/config.rb
+++ lib/work_md/config.rb
@@ -5,7 +5,6 @@ require 'yaml'
 module WorkMd
   module Config
     DEFAULT_WORK_DIR = Dir.home + '/work_md'
-    YAML_FILE = YAML.load_file(DEFAULT_WORK_DIR + '/config.yml')
     DEFAULT_EDITOR = 'vi'
     TRANSLATIONS = {
       'pt' =>
@@ -31,11 +30,11 @@ module WorkMd
     }.freeze
 
     def self.title
-      YAML_FILE['title'] || ''
+      yaml_file['title'] || ''
     end
 
     def self.editor
-      ENV['EDITOR'] || ENV['VISUAL'] || YAML_FILE['editor'] || DEFAULT_EDITOR
+      ENV['EDITOR'] || ENV['VISUAL'] || yaml_file['editor'] || DEFAULT_EDITOR
     end
 
     def self.work_dir
@@ -44,8 +43,14 @@ module WorkMd
 
     def self.translations
       TRANSLATIONS[ENV['WORK_MD_LANG']] ||
-        TRANSLATIONS[YAML_FILE['lang']] ||
+        TRANSLATIONS[yaml_file['lang']] ||
         TRANSLATIONS['en']
+    end
+
+    def self.yaml_file
+      YAML.load_file(DEFAULT_WORK_DIR + '/config.yml')
+    rescue StandardError
+      {}
     end
   end
 end
