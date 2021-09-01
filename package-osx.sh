b97cd21e1979fa69bcec28f35921c51e41831b71
Henrique F. Teixeira <henrique.teixeira@rdstation.com>
2021-09-01 20:24:20 -0300

    feat(charts): Add first win version and bump version

--- package-osx.sh
+++ package-osx.sh
@@ -1,10 +1,10 @@
 # gem
 cd packaging && curl -L -O --fail http://d6r77u77i8pq3.cloudfront.net/releases/traveling-ruby-20210206-2.4.10-osx.tar.gz
 cd ..
-mkdir work_md-0.3.2-osx
-mkdir work_md-0.3.2-osx/ruby
-tar -xzf packaging/traveling-ruby-20210206-2.4.10-osx.tar.gz -C work_md-0.3.2-osx/ruby
-cp -r gem work_md-0.3.2-osx/gem
+mkdir work_md-0.3.3-osx
+mkdir work_md-0.3.3-osx/ruby
+tar -xzf packaging/traveling-ruby-20210206-2.4.10-osx.tar.gz -C work_md-0.3.3-osx/ruby
+cp -r gem work_md-0.3.3-osx/gem
 
 # dependency gems
 mkdir packaging/tmp
@@ -15,18 +15,18 @@ BUNDLE_IGNORE_CONFIG=1 bundle install --path ../vendor --without development
 cd ../..
 rm -rf packaging/tmp
 rm -f packaging/vendor/*/*/cache/*
-cp -r packaging/vendor work_md-0.3.2-osx/vendor
-mkdir work_md-0.3.2-osx/vendor/.bundle
-cp packaging/bundler-config work_md-0.3.2-osx/vendor/.bundle/config
+cp -r packaging/vendor work_md-0.3.3-osx/vendor
+mkdir work_md-0.3.3-osx/vendor/.bundle
+cp packaging/bundler-config work_md-0.3.3-osx/vendor/.bundle/config
 
-cp packaging/Gemfile work_md-0.3.2-osx/vendor/Gemfile
-cp packaging/Gemfile.lock work_md-0.3.2-osx/vendor/Gemfile.lock
+cp packaging/Gemfile work_md-0.3.3-osx/vendor/Gemfile
+cp packaging/Gemfile.lock work_md-0.3.3-osx/vendor/Gemfile.lock
 
-cp packaging/run.sh work_md-0.3.2-osx/work_md
-chmod +x work_md-0.3.2-osx/work_md
+cp packaging/run.sh work_md-0.3.3-osx/work_md
+chmod +x work_md-0.3.3-osx/work_md
 rm -r vendor
 rm packaging/Gemfile.lock
 
 # package
-tar -czf work_md-0.3.2-osx.tar.gz work_md-0.3.2-osx
-rm -rf work_md-0.3.2-osx
+tar -czf work_md-0.3.3-osx.tar.gz work_md-0.3.3-osx
+rm -rf work_md-0.3.3-osx
