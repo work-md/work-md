15016c131a4c362d4d7764a2b3bd53414a32d424
Henrique F. Teixeira <henrique.teixeira@rdstation.com>
2021-09-17 14:40:20 -0300

    feat(master): Add packages

--- package-osx.sh
+++ package-osx.sh
@@ -1,10 +1,10 @@
 # gem
 cd packaging && curl -L -O --fail http://d6r77u77i8pq3.cloudfront.net/releases/traveling-ruby-20210206-2.4.10-osx.tar.gz
 cd ..
-mkdir work-md-0.3.4-osx
-mkdir work-md-0.3.4-osx/ruby
-tar -xzf packaging/traveling-ruby-20210206-2.4.10-osx.tar.gz -C work-md-0.3.4-osx/ruby
-cp -r gem work-md-0.3.4-osx/gem
+mkdir work-md-0.3.9-osx
+mkdir work-md-0.3.9-osx/ruby
+tar -xzf packaging/traveling-ruby-20210206-2.4.10-osx.tar.gz -C work-md-0.3.9-osx/ruby
+cp -r gem work-md-0.3.9-osx/gem
 
 # dependency gems
 mkdir packaging/tmp
@@ -15,17 +15,17 @@ BUNDLE_IGNORE_CONFIG=1 bundle install --path ../vendor --without development
 cd ../..
 rm -rf packaging/tmp
 rm -f packaging/vendor/*/*/cache/*
-cp -r packaging/vendor work-md-0.3.4-osx/vendor
-mkdir work-md-0.3.4-osx/vendor/.bundle
-cp packaging/bundler-config work-md-0.3.4-osx/vendor/.bundle/config
+cp -r packaging/vendor work-md-0.3.9-osx/vendor
+mkdir work-md-0.3.9-osx/vendor/.bundle
+cp packaging/bundler-config work-md-0.3.9-osx/vendor/.bundle/config
 
-cp packaging/Gemfile work-md-0.3.4-osx/vendor/Gemfile
-cp packaging/Gemfile.lock work-md-0.3.4-osx/vendor/Gemfile.lock
+cp packaging/Gemfile work-md-0.3.9-osx/vendor/Gemfile
+cp packaging/Gemfile.lock work-md-0.3.9-osx/vendor/Gemfile.lock
 
-cp packaging/run.sh work-md-0.3.4-osx/work-md
-chmod +x work-md-0.3.4-osx/work-md
+cp packaging/run.sh work-md-0.3.9-osx/work-md
+chmod +x work-md-0.3.9-osx/work-md
 rm -r vendor
 rm packaging/Gemfile.lock
 
 # package
-# tar -czf work-md-0.3.4-osx.tar.gz work-md-0.3.4-osx && rm -rf work-md-0.3.4-osx
+# tar -czf work-md-0.3.9-osx.tar.gz work-md-0.3.9-osx && rm -rf work-md-0.3.9-osx
